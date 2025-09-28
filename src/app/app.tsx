8e736a897fde9dbb819c87bc5ba0a8a9ec2c0e3a
KrzyMru <krzymru20@gmail.com>
2025-09-28 20:59:22 +0200

    Main page styling

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,48 +6,55 @@ import Scissors from '../assets/images/icon-scissors.svg';
 
 const App = () => {
   return (
-    <main>
-      <header>
+    <main className='page'>
+      <header className='header'>
         <img 
           src={Logo}
           alt='logo'
+          className='header__logo'
         />
-        <div>
-          <span>SCORE</span>
-          <span>12</span>
+        <div className='scoreboard'>
+          <span className='scoreboard__text'>SCORE</span>
+          <span className='scoreboard__number'>12</span>
         </div>
       </header>
-      <div>
+      <div className='shapes'>
         <button
           type='button'
           title='Paper'
+          className='shape__container shape__container--blue shape__container--top-left'
         >
-          <div>
+          <div className='shape__background'>
             <img 
               src={Paper}
               alt='paper'
+              className='shape__logo'
             />
           </div>
         </button>
         <button
           type='button'
           title='Scissors'
+          className='shape__container shape__container--gold shape__container--top-right'
         >
-          <div>
+          <div className='shape__background'>
             <img 
               src={Scissors}
               alt='scissors'
+              className='shape__logo'
             />
           </div>
         </button>
         <button
           type='button'
           title='Rock'
+          className='shape__container shape__container--red shape__container--bottom-middle'
         >
-          <div>
+          <div className='shape__background'>
             <img 
               src={Rock}
               alt='rock'
+              className='shape__logo'
             />
           </div>
         </button>
@@ -55,6 +62,7 @@ const App = () => {
       <button
         type='button'
         title='Rules'
+        className='rules'
       >
         RULES
       </button>
