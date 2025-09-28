a5d8b71af71ff5fe03ae74091a0c7b8fd4ee5eb9
KrzyMru <krzymru20@gmail.com>
2025-09-28 18:03:31 +0200

    Basic HTML structure

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,64 @@
 import './app.css';
+import Logo from '../assets/images/logo.svg';
+import Rock from '../assets/images/icon-rock.svg';
+import Paper from '../assets/images/icon-paper.svg';
+import Scissors from '../assets/images/icon-scissors.svg';
 
 const App = () => {
   return (
-    <></>
+    <main>
+      <header>
+        <img 
+          src={Logo}
+          alt='logo'
+        />
+        <div>
+          <span>SCORE</span>
+          <span>12</span>
+        </div>
+      </header>
+      <div>
+        <button
+          type='button'
+          title='Paper'
+        >
+          <div>
+            <img 
+              src={Paper}
+              alt='paper'
+            />
+          </div>
+        </button>
+        <button
+          type='button'
+          title='Scissors'
+        >
+          <div>
+            <img 
+              src={Scissors}
+              alt='scissors'
+            />
+          </div>
+        </button>
+        <button
+          type='button'
+          title='Rock'
+        >
+          <div>
+            <img 
+              src={Rock}
+              alt='rock'
+            />
+          </div>
+        </button>
+      </div>
+      <button
+        type='button'
+        title='Rules'
+      >
+        RULES
+      </button>
+    </main>
   )
 }
 
